27642b245462d2897123973b56a90e63ae2f496b
Bikkukbabu <56974346+Bikkukbabu@users.noreply.github.com>
2025-12-29 15:11:15 +0530

    Time: 11 ms (82.78%) | Memory: 19.9 MB (97.21%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,13 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         string_mapping = {}
-        result = []
         for string in strs:
-            map_string = "".join(sorted(string))
-            if map_string in string_mapping:
-                string_mapping[map_string].append(string)
+            sorted_string = "".join(sorted(string))
+            if sorted_string in string_mapping:
+                string_mapping[sorted_string].extend([string])
             else:
-                string_mapping[map_string] = [string]
+                string_mapping[sorted_string] = [string]
+        result = []
         for _, value in string_mapping.items():
             result.append(value)
         return result
