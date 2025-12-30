bbc41d7e52d202788ae1826f5653348c000cd8f9
Bikkukbabu <56974346+Bikkukbabu@users.noreply.github.com>
2025-12-30 21:24:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (92.68%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,5 +4,4 @@ class Solution:
         len_array = len(digits)
         for i in range(len_array, 0, -1):
             number = number + digits[len_array-i]*(10**(i-1))
-        result = [int(d) for d in str(number+1)]
-        return result
+        return [int(d) for d in str(number+1)]
