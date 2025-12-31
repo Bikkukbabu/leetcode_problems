1f5bc5aec6238de73c1f47b4317ea1107b9f2883
Bikkukbabu <56974346+Bikkukbabu@users.noreply.github.com>
2025-12-31 11:42:07 +0530

    Time: 30 ms (12.75%) | Memory: 17.6 MB (98.53%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,16 +1,13 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomnote_map = {}
         magazine_map = {}
-        for index in range(len(ransomNote)):
-            char = ransomNote[index]
-            ransomnote_map[char] = ransomnote_map.get(char, 0) + 1
         for index in range(len(magazine)):
             char = magazine[index]
             magazine_map[char] = magazine_map.get(char, 0) + 1
-        for key, value in ransomnote_map.items():
-            if magazine_map.get(key, 0) == 0:
+        for index in range(len(ransomNote)):
+            char = ransomNote[index]
+            if magazine_map.get(char, 0) == 0:
                 return False
-            if magazine_map.get(key) < value:
-                return False
+            else:
+                magazine_map[char] = magazine_map.get(char) - 1
         return True
