ae2bb9fa1a01ee4cab7e081e36c767e15be2a463
Bikkukbabu <56974346+Bikkukbabu@users.noreply.github.com>
2026-01-03 16:29:26 +0530

    Time: 12 ms (51.77%) | Memory: 17.3 MB (99.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if sorted(s) == sorted(t):
-            return True
-        return False
+        s_mapping = {}
+        if len(s) != len(t):
+            return False
+        for char in s:
+            s_mapping[char] = s_mapping.get(char, 0) + 1
+        for c in t:
+            if s_mapping.get(c, 0) == 0:
+                return False
+            else:
+                s_mapping[c] = s_mapping[c] - 1
+        return True
+            
